4ba8bf54b91fd0fec6c7f1c814408ae78e7e9798
ccat <deadlack@gmail.com>
2014-08-03 16:45:41 +0900

    test added

--- day1/set_test.py
+++ day1/set_test.py
@@ -10,20 +10,55 @@ class ExtensionalDefinitionTest(unittest.TestCase):
 
     def test_extensional_definition(self):
         setA = Set([1,2,3,4,5])
+        self.assertTrue(setA.is_element(1))
+        self.assertTrue(1 in setA)
 
     def test_union(self):
         setA = Set([1,2,3,4,5])
+        setB = Set([4,5,6,7,8,9,10])
 
-        self.assertTrue(element in self.seq)
+        setC = setA.union(setB)
+        for i in range(1,10):
+            self.assertTrue(i in setC)
+
+        setC = setA + setB
+        for i in range(1,10):
+            self.assertTrue(i in setC)
 
     def test_intersection(self):
         setA = Set([1,2,3,4,5])
+        setB = Set([4,5,6,7,8,9,10])
+
+        setC = setA.intersection(setB)
+        for i in range(1,3):
+            self.assertTrue(not i in setC)
+        for i in range(4,5):
+            self.assertTrue(i in setC)
+        for i in range(6,10):
+            self.assertTrue(not i in setC)
 
     def test_complement(self):
         setA = Set([1,2,3,4,5])
+        setB = Set([4,5,6,7,8,9,10])
+
+        setC = setA.complement(setB)
+        for i in range(1,3):
+            self.assertTrue(i in setC)
+        for i in range(4,10):
+            self.assertTrue(not i in setC)
+
+        setC = setA - setB
+        for i in range(1,3):
+            self.assertTrue(i in setC)
+        for i in range(4,10):
+            self.assertTrue(not i in setC)
 
     def test_relation(self):
         setA = Set([1,2,3,4,5])
+        setB = Set([4,5,6,7,8,9,10])
 
-
+        setC = setA.product(setB)
+        for x in range(1,5):
+            for y in range(4,10):
+                self.assertTrue((x,y) in setC)
 
