127d918647065bb76d55419d7374e132d4fea738
ccat <deadlack@gmail.com>
2014-08-03 16:24:00 +0900

    test commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 TinyDatabase
 ============
+
+TinyDatabaseはデータベースに関する学習用のソース群です。
+全体的な説明は下記に記載しています。
+https://www.whiteblack-cat.info/ja/notes/info_theory/database/
+
+なお、言語にはPythonを利用しています。
+
+## Licence
+
+LICENSEファイル参照
+
+## Author
+
+[ccat](https://github.com/ccat)
+
+
---
+++ day1/set_test.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+import set
+
+class ExtensionalDefinitionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_extensional_definition(self):
+        setA = Set([1,2,3,4,5])
+
+    def test_union(self):
+        setA = Set([1,2,3,4,5])
+
+        self.assertTrue(element in self.seq)
+
+    def test_intersection(self):
+        setA = Set([1,2,3,4,5])
+
+    def test_complement(self):
+        setA = Set([1,2,3,4,5])
+
+    def test_relation(self):
+        setA = Set([1,2,3,4,5])
+
+
+
