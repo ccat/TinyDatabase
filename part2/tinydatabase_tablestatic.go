ba2201c07bd14339e59f06bf6762dae12ac490e4
ccat <mailaddress.not.found@gmail.com>
2016-08-21 02:35:40 +0900

    backup commit

---
+++ part2/tinydatabase_tablestatic.go
@@ -0,0 +1,334 @@
+package tinydatabase
+
+import (
+	//"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	//"fmt"
+	"io"
+	"io/ioutil"
+	//"math"
+	"os"
+	"path"
+	//"strconv"
+	//"time"
+)
+
+type TableStatic struct {
+	tablefile   *os.File
+	fileVersion int64
+	columnTypes []ColumnType
+	columnBytes int64
+}
+
+/*
+ NewTable func creates table file and config file.
+ When files exist, returns error.
+*/
+func (self *TableStatic) NewTable(directory string, tablename string, columnTypes []ColumnType) error {
+	directory = path.Clean(directory)
+	directory = directory + "/"
+	dCheck, err := os.Stat(directory)
+	dCheck = dCheck
+	if err != nil {
+		return err
+	}
+	if dCheck.IsDir() == false {
+		return errors.New("Directory does not exist.")
+	}
+
+	_, err = os.Stat(directory + tablename + ".config")
+	if err == nil {
+		return errors.New("Config file exists.")
+	}
+	_, err = os.Stat(directory + tablename + ".table")
+	if err == nil {
+		return errors.New("Table file exists.")
+	}
+	err = self.Close()
+	if err != nil {
+		return err
+	}
+
+	err = self.setColumns(columnTypes)
+	if err != nil {
+		return err
+	}
+	err = self.saveConfigFile(directory + tablename + ".config")
+	if err != nil {
+		return err
+	}
+
+	err = self.openTableFile(directory + tablename + ".table")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+ Open func opens table file and config file.
+*/
+func (self *TableStatic) Open(directory string, tablename string) error {
+	err := self.Close()
+	if err != nil {
+		return err
+	}
+	directory = path.Clean(directory)
+	directory = directory + "/"
+	err = self.openConfigFile(directory + tablename + ".config")
+	if err != nil {
+		return err
+	}
+	err = self.openTableFile(directory + tablename + ".table")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (self *TableStatic) Close() error {
+	if self.tablefile == nil {
+		return nil
+	}
+	err := self.tablefile.Close()
+	if err != nil {
+		return err
+	}
+	self.tablefile = nil
+	return nil
+}
+
+/*
+ WriteRow func writes row on table file.
+ When rowNum is -1, new row will be created.
+*/
+func (self *TableStatic) WriteRow(rowNum int64, row Row) (int64, error) {
+	lastRowNum, err := self.searchLastRowNum()
+	if err != nil {
+		return -1, err
+	}
+	if rowNum == -1 || rowNum > lastRowNum {
+		rowNum = lastRowNum
+	}
+	targetOff := self.convertRowNumToOffset(rowNum)
+	var b []byte
+	b = make([]byte, 1)
+	b[0] = ROW_NORMAL
+	num, err := self.tablefile.WriteAt(b, targetOff)
+	if err != nil {
+		return -1, err
+	}
+	targetOff = targetOff + int64(num)
+	for _, v := range self.columnTypes {
+		if val, ok := row[v.Name]; ok {
+			b, err = v.ConvertToBytes(val)
+			if err != nil {
+				return -1, err
+			}
+		} else {
+			b, err = v.GetNil()
+			if err != nil {
+				return -1, err
+			}
+		}
+		num, err := self.tablefile.WriteAt(b, targetOff)
+		if err != nil {
+			return -1, err
+		}
+		targetOff = targetOff + int64(num)
+	}
+	err = self.tablefile.Sync()
+	if err != nil {
+		return -1, err
+	}
+	return rowNum, nil
+}
+
+func (self *TableStatic) ReadRow(rowNum int64) (Row, error) {
+	lastRowNum, err := self.searchLastRowNum()
+	if err != nil {
+		return nil, err
+	}
+	if rowNum > lastRowNum {
+		return nil, errors.New("Out of Row index")
+	}
+	if rowNum < 0 {
+		return nil, errors.New("Out of Row index")
+	}
+	targetOff := self.convertRowNumToOffset(rowNum)
+
+	var b []byte
+	b = make([]byte, 1)
+	_, err = self.tablefile.ReadAt(b, targetOff)
+	if err != nil {
+		return nil, err
+	}
+	if b[0] == ROW_DELETED {
+		return nil, errors.New("Deleted row")
+	}
+	targetOff = targetOff + 1
+
+	result := make(Row)
+	for _, v := range self.columnTypes {
+		b, err = v.GetNil()
+		if err != nil {
+			return nil, err
+		}
+		_, err = self.tablefile.ReadAt(b, targetOff)
+		if err != nil {
+			return nil, err
+		}
+		result[v.Name], err = v.ConvertToVal(b)
+		if err != nil {
+			return nil, err
+		}
+		nextOff, err := v.GetBytes()
+		if err != nil {
+			return nil, err
+		}
+		targetOff = targetOff + int64(nextOff)
+	}
+	return result, nil
+}
+
+func (self *TableStatic) DeleteRow(rowNum int64) error {
+	lastRowNum, err := self.searchLastRowNum()
+	if err != nil {
+		return err
+	}
+	if rowNum > lastRowNum {
+		return errors.New("Out of Row index")
+	}
+	if rowNum < 0 {
+		return errors.New("Out of Row index")
+	}
+
+	targetOff := self.convertRowNumToOffset(rowNum)
+	var b []byte
+	b = make([]byte, 1)
+	b[0] = ROW_DELETED
+	_, err = self.tablefile.WriteAt(b, targetOff)
+	err = self.tablefile.Sync()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//**************************************************
+
+func (self *TableStatic) openConfigFile(configfilename string) error {
+
+	jsonString, err := ioutil.ReadFile(configfilename)
+	if err != nil {
+		return err
+	}
+
+	var columnTypes []ColumnType
+	err = json.Unmarshal(jsonString, &columnTypes)
+	if err != nil {
+		return err
+	}
+	err = self.setColumns(columnTypes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func (self *TableStatic) openTableFile(tablefilename string) error {
+	f, err := os.OpenFile(tablefilename, os.O_RDWR+os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	self.tablefile = f
+
+	b := make([]byte, binary.MaxVarintLen64)
+	num, err := self.tablefile.ReadAt(b, 0)
+	if err != nil {
+		if err == io.EOF {
+			self.fileVersion = STATIC1
+			binary.PutVarint(b, self.fileVersion)
+			num, err = self.tablefile.WriteAt(b, 0)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	} else {
+		self.fileVersion, num = binary.Varint(b)
+		if num == 0 {
+			return errors.New("Failed to read fileversion")
+		}
+		if self.fileVersion != STATIC1 {
+			return errors.New("Fileversion is not correct")
+		}
+	}
+
+	return err
+}
+
+func (self *TableStatic) saveConfigFile(configfile string) error {
+	b, err := json.Marshal(self.columnTypes)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(configfile, b, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (self *TableStatic) setColumns(columnTypes []ColumnType) error {
+	columnBytes := int64(0)
+	flags := map[string]int{}
+
+	for _, val := range columnTypes {
+		_, ok := flags[val.Name]
+		if ok == true {
+			return errors.New("Same column name exists.")
+		}
+		flags[val.Name] = 1
+		num, err := val.GetBytes()
+		if err != nil {
+			return err
+		}
+		val.Size = num
+		columnBytes = columnBytes + num
+	}
+	self.columnTypes = columnTypes
+	self.columnBytes = columnBytes
+	return nil
+}
+
+func (self *TableStatic) convertRowNumToOffset(rowNum int64) int64 {
+	offset := int64(rowNum)*int64(self.columnBytes+1) + int64(binary.MaxVarintLen64)
+	return offset
+}
+func (self *TableStatic) convertOffsetToRowNum(offset int64) int64 {
+	rowNum := int64((offset - int64(binary.MaxVarintLen64)) / (int64(self.columnBytes + 1)))
+	return rowNum
+}
+
+func (self *TableStatic) searchLastRowNum() (int64, error) {
+	lastOff, err := self.tablefile.Seek(0, 2)
+	if err != nil {
+		return -1, err
+	}
+	rowNum := self.convertOffsetToRowNum(lastOff)
+	return rowNum, err
+}
+
+/*func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil
+}
+
+func (self TinyDatabaseError) Error() string {
+	return self.Message
+}*/
---
+++ part2/tinydatabase_tablestatic_test.go
@@ -0,0 +1,292 @@
+package tinydatabase
+
+import (
+	//"fmt"
+	"os"
+	//"path"
+	"strings"
+	"testing"
+	"time"
+)
+
+func Test1_TableStatic_basicUsage(t *testing.T) {
+	directory := "./"
+	tablename := "test"
+	os.Remove(directory + tablename + ".table")
+	os.Remove(directory + tablename + ".config")
+	columnSet := []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: 256},
+		{Name: "dateline", Type: "time", Size: 15},
+	}
+
+	var tableInst TableInterface
+	tableInst = &TableStatic{}
+	err := tableInst.NewTable(directory, tablename, columnSet)
+
+	if err != nil {
+		t.Errorf("Failed to create table: %s", err)
+	}
+	_, err = os.Stat(tablename + ".table")
+	if err != nil {
+		t.Errorf("Failed to create table file:%s", err)
+	}
+	_, err = os.Stat(tablename + ".config")
+	if err != nil {
+		t.Errorf("Failed to create config file:%s", err)
+	}
+	err = tableInst.Open(directory, tablename)
+
+	if err != nil {
+		t.Errorf("Failed to open table: %s", err)
+	}
+
+	testRow := make(Row)
+	testRow["intline"] = int64(100)
+	testRow["floatline"] = 10.5
+	testRow["strline"] = "aaaa"
+	testRow["dateline"] = time.Now()
+
+	num, err := tableInst.WriteRow(-1, testRow)
+	if err != nil {
+		t.Errorf("Failed to insert row: %s", err)
+	}
+	if num != 0 {
+		t.Errorf("Failed to insert row at 0: %d", num)
+	}
+
+	testRow["floatline"] = 12.5
+	num, err = tableInst.WriteRow(-1, testRow)
+	if err != nil {
+		t.Errorf("Failed to insert row: %s", err)
+	}
+	if num != 1 {
+		t.Errorf("Failed to insert row at 1:%d", num)
+	}
+
+	num = 0
+	testRow["floatline"] = 10.5
+	testRow2, err := tableInst.ReadRow(num)
+	if err != nil {
+		t.Errorf("Failed to read row at 0: %s", err)
+	}
+	if testRow2["intline"] != testRow["intline"] {
+		t.Errorf("Failed to read row at 0: intline")
+	}
+	if testRow2["floatline"] != testRow["floatline"] {
+		t.Errorf("Failed to read row at 0: floatline")
+	}
+	str1, _ := testRow["strline"].(string)
+	str2, _ := testRow2["strline"].(string)
+	if str2 != str1 {
+		t.Errorf("Failed to read row at 0: strline: %s!=%s", str2, str1)
+		t.Errorf("len testMap:%d", len(str1))
+		t.Errorf("len testMap2:%d", len(str2))
+	}
+	if testRow2["dateline"] != testRow["dateline"] {
+		t.Errorf("Failed to read row at 0: dateline")
+	}
+
+	testRow2["intline"] = int64(102)
+	_, err = tableInst.WriteRow(num, testRow2)
+	if err != nil {
+		t.Errorf("Failed to update row at 0: %s", err)
+	}
+	testRow3, err := tableInst.ReadRow(num)
+	if testRow2["intline"] != testRow3["intline"] {
+		t.Errorf("Failed to update row at 0: intline")
+	}
+
+	err = tableInst.DeleteRow(num)
+	if err != nil {
+		t.Errorf("Failed to delete row at 0: %s", err)
+	}
+	_, err = tableInst.ReadRow(num)
+	if err.Error() != "Deleted row" {
+		t.Errorf("Failed to delete row at 0: %s", err)
+	}
+
+	testRow2["intline"] = "string data"
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err == nil {
+		t.Errorf("Failed to check invalid data")
+	}
+	if err != nil && strings.HasPrefix(err.Error(), "Missmatch type(int64)") == false {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+
+	testRow2["intline"] = 100
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err != nil {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	testRow2["intline"] = int32(100)
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err != nil {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	testRow2["intline"] = int16(100)
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err != nil {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	testRow2["intline"] = int8(100)
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err != nil {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+
+	testRow2["intline"] = 100.2
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err == nil {
+		t.Errorf("Failed to check invalid data")
+	}
+	if err != nil && strings.HasPrefix(err.Error(), "Missmatch type(int64)") == false {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	testRow2["intline"] = int64(100)
+	testRow2["strline"] = 100
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err == nil {
+		t.Errorf("Failed to check invalid data")
+	}
+	if err != nil && strings.HasPrefix(err.Error(), "Missmatch type(string)") == false {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	testRow2["strline"] = 100.2
+	_, err = tableInst.WriteRow(-1, testRow2)
+	if err == nil {
+		t.Errorf("Failed to check invalid data")
+	}
+	if err != nil && strings.HasPrefix(err.Error(), "Missmatch type(string)") == false {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+
+	testRow2["strline"] = "test"
+	num, err = tableInst.WriteRow(1000, testRow2)
+	if err != nil {
+		t.Errorf("Failed to check invalid data: %s", err)
+	}
+	if num == 1000 {
+		t.Errorf("Failed to change row num")
+	}
+
+	_, err = tableInst.ReadRow(1000)
+	if err == nil {
+		t.Errorf("Failed to raise error for invalid row")
+	}
+
+	err = tableInst.DeleteRow(1000)
+	if err == nil {
+		t.Errorf("Failed to raise error for invalid row")
+	}
+
+	_, err = tableInst.ReadRow(-1)
+	if err == nil {
+		t.Errorf("Failed to raise error for invalid row")
+	}
+
+	err = tableInst.DeleteRow(-1)
+	if err == nil {
+		t.Errorf("Failed to raise error for invalid row")
+	}
+
+	tableInst.Close()
+
+}
+
+func Test2_TableStatic_errUsage(t *testing.T) {
+	directory := "./"
+	tablename := "test"
+	os.Remove(tablename + ".table")
+	os.Remove(tablename + ".config")
+
+	columnSet := []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: 256},
+		{Name: "dateline", Type: "time2", Size: 15},
+	}
+
+	var tableInst TableInterface
+	tableInst = &TableStatic{}
+	err := tableInst.NewTable(directory, tablename, columnSet)
+
+	if err == nil {
+		t.Errorf("Failed to return column type error")
+	}
+
+	columnSet = []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: 256},
+		{Name: "dateline", Type: "time", Size: 15},
+		{Name: "strline", Type: "string", Size: 256},
+	}
+
+	err = tableInst.NewTable(directory, tablename, columnSet)
+
+	if err == nil {
+		t.Errorf("Failed to return column config error")
+	}
+
+	directory = "./notexistdir"
+	columnSet = []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: 10},
+		{Name: "dateline", Type: "time", Size: 15},
+	}
+
+	err = tableInst.NewTable(directory, tablename, columnSet)
+
+	if err == nil {
+		t.Errorf("Failed to return directory check error")
+	}
+
+	directory = "./"
+	columnSet = []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: -1},
+		{Name: "dateline", Type: "time", Size: 15},
+	}
+
+	err = tableInst.NewTable(directory, tablename, columnSet)
+
+	if err == nil {
+		t.Errorf("Failed to return size check error")
+	}
+
+	columnSet = []ColumnType{
+		{Name: "intline", Type: "int64", Size: 64},
+		{Name: "floatline", Type: "float64", Size: 64},
+		{Name: "strline", Type: "string", Size: 10},
+		{Name: "dateline", Type: "time", Size: 15},
+	}
+
+	err = tableInst.NewTable(directory, tablename, columnSet)
+	if err != nil {
+		t.Errorf("Failed to create new table:%s", err)
+	}
+
+	testRow := make(Row)
+	testRow["intline"] = int64(100)
+	testRow["floatline"] = 10.5
+	testRow["strline"] = "This is over than 10 words"
+	testRow["dateline"] = time.Now()
+
+	_, err = tableInst.WriteRow(-1, testRow)
+	if err == nil {
+		t.Errorf("Failed to check string count")
+	}
+
+	err = tableInst.NewTable(directory, tablename, columnSet)
+	if err == nil {
+		t.Errorf("Failed to check file overwrite")
+	}
+
+	tableInst.Close()
+
+}
